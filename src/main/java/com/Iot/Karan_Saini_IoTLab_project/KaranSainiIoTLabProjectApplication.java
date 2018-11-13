96041d09819857927ace2702940720c99ed2213d
KaranSaini92837 <37518846+KaranSaini92837@users.noreply.github.com>
2018-11-13 17:28:52 -0500

    Project Initialization

---
+++ src/main/java/com/Iot/Karan_Saini_IoTLab_project/KaranSainiIoTLabProjectApplication.java
@@ -0,0 +1,12 @@
+package com.Iot.Karan_Saini_IoTLab_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaranSainiIoTLabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaranSainiIoTLabProjectApplication.class, args);
+	}
+}
